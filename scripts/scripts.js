84dc074d825e5d2563a93b1a0a4714590b1e4ff2
Qwant50 <qwantonline@gmail.com>
2016-05-14 20:40:53 +0300

    ref photo script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,3 +6,13 @@
 //		return false;
 //	});
 //});
+
+
+$('#content img').on('click', function () {
+    var src = $(this).attr('src');
+    var image = new Image();
+    image.src = src;
+    var width = image.width;
+    var height = image.height;
+    window.open(src, "Image", "width=" + width + ",height=" + height);
+});
