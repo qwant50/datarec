6793a2ee5cb61fef8e40281617189a6087183491
Qwant50 <qwantonline@gmail.com>
2016-06-08 22:51:44 +0300

    ref delivery

--- app/controllers/AjaxController.php
+++ app/controllers/AjaxController.php
@@ -8,13 +8,13 @@ class ajaxController
 {
     public function deliveryAction()
     {
-        $db = DbConnect::getInstance();
-        $stmt = $db->getConnection()->prepare("SET NAMES utf8;");
+        $db = DbConnect::getInstance()->getConnection();
+        $stmt = $db->prepare("SET NAMES utf8;");
         $stmt->execute();
-/*        $stmt = $db->getConnection()->prepare("INSERT INTO `qwant50` (`number`, `sign`, `message`, `wappush`, `is_flash`, `send_time`)
+/*        $stmt = $db->prepare("INSERT INTO `qwant50` (`number`, `sign`, `message`, `wappush`, `is_flash`, `send_time`)
 VALUES ('380675578857', 'datarec', 'Ваш заказ 1544 готов', '', 0, NOW());");
         $stmt->execute();*/
-        $stmt = $db->getConnection()->prepare("SELECT * FROM `qwant50`");
+        $stmt = $db->prepare("SELECT * FROM `qwant50`");
         $stmt->execute();
         $records = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         var_dump($records);
