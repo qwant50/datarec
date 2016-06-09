7b238a9f3257f7ceeca92c7b08bb2fb7fd0a5c91
Qwant50 <qwantonline@gmail.com>
2016-06-09 20:51:26 +0300

    http/s  relink to //

--- php/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-$command = 'cd ../../../../var ; ls -l';
-$a = `{$command}`;
-$a = preg_replace('/[\t\r\n]/', '<br>', $a);
-$a = preg_replace('/ /', '&nbsp', $a);
-$a = '<pre>' . $a . '</pre>';
-echo $a;
-
