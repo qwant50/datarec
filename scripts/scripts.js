5138d151a231fb7b263cb4933b3eb602671d745b
Qwant50 <qwantonline@gmail.com>
2016-07-31 20:56:50 +0300

    add bootstrap

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,3 +16,37 @@ $('#content img').on('click', function () {
     var height = image.height;
     window.open(src, "Image", "width=" + width + ",height=" + height);
 });
+
+$("#dialog").dialog({
+    autoOpen: false,
+    width: 500
+});
+
+$('button[type=submit]').click(function () {
+    var val = $('input[name=number]').val();
+    $('#dialog').dialog("option", "title", 'Сведение о состоянии ремонта № ' + val);
+    $.ajax({
+        method: "POST",
+        url: "http://admin.datarec.com.ua/admin/repair/getstatus",
+        dataType: "json",
+        data: {'id': val}
+    }).done(function (res) {
+            var rep = '';
+            if (res.repaired_at === null) {
+                rep = 'Статус: не готово';
+            }
+            else {
+                rep = 'Статус: готово <br> Дата завершения: ' + res.repaired_at;
+            }
+            $('#dialog').html('Изделие: ' + res.device + '<br>' + rep);
+            $("#dialog").dialog("open");
+        })
+        .fail(function () {
+            alert("error");
+        })
+        .always(function (res) {
+            console.log(res);
+            //  alert(res);
+        });
+
+});
